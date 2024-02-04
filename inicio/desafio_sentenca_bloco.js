a7748318ffce353f714085f18ff39f2a0bb0b0d4
Janaína Gomes <jana.gomes90@hotmail.com>
2024-02-04 19:37:20 -0300

    up

---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,21 @@
+/* exibir no console
+* número impares de 1 - 10
+* e depois os pares.
+* em dois blocos separados
+*/
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
