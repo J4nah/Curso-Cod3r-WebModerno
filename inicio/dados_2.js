cdbcae58a2503b35f5ba8b6a350ab732eea5dfab
Janaína Gomes <jana.gomes90@hotmail.com>
2024-02-04 19:52:10 -0300

    att

---
+++ inicio/dados_2.js
@@ -0,0 +1,9 @@
+// usar nomes para as variaveis que tenham coerencia.
+
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.9 * 0.6);
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
