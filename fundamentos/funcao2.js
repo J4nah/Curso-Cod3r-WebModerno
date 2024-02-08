0be02d0a8e0852acd8c3ba68651d999ab8633c59
Janaína Gomes <jana.gomes90@hotmail.com>
2024-02-07 22:33:54 -0300

    up

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+// Atmazenando uma função em uma variavel
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(4,5)
+
+// Armazenando uma função arrow em uma variavel
+
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2,3))
+
+//retorno implicito 
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(3,2))
