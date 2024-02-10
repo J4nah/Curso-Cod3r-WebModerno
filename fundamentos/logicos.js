a3a846ecf21f5f64136bd9e15c8ee97bc43b20c6
Janaína Gomes <jana.gomes90@hotmail.com>
2024-02-10 14:05:13 -0300

    att

---
+++ fundamentos/logicos.js
@@ -0,0 +1,10 @@
+function compras (trab1, trab2) {
+    const comprarSorvete = trab1 || trab2
+    const comprarTv50 = trab1 && trab2
+    const comprarTv32 = trab1 != trab2
+    const manterSaudavel = !comprarSorvete
+
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel}
+}
+
+console.log(compras(false,true))
---
+++ fundamentos/unarios.js
@@ -0,0 +1,9 @@
+let num1 = 1
+let num2 = 2
+
+num1++ //posfix
+console.log(num1)
+
+--num1 //prefix
+console.log(num1)
+
